2121e8762be2a2ba61166d74700c45daf8b0a99a
YUZAWA Takahiko <yuzawataka@intellilink.co.jp>
2012-01-11 16:58:26 +0900

    delete unused code.

--- keystone/keystone/middleware/auth_token_for_colony.py
+++ keystone/keystone/middleware/auth_token_for_colony.py
@@ -74,7 +74,7 @@ from paste.deploy import loadapp
 from time import time
 from urllib import unquote
 from urlparse import urlparse
-from webob.exc import HTTPUnauthorized, HTTPUseProxy, HTTPForbidden, HTTPUnauthorized
+from webob.exc import HTTPUnauthorized, HTTPUseProxy, HTTPForbidden, HTTPUnauthorized, HTTPNotFound
 from webob import Request, Response
 import keystone.tools.tracer  # @UnusedImport # module runs on import
 
@@ -88,56 +88,8 @@ PROTOCOL_NAME = "Token Authentication"
 class AuthProtocol(object):
     """Auth Middleware that handles authenticating client calls"""
 
-    def _init_protocol_common(self, app, conf):
-        """ Common initialization code"""
-        print "Starting the %s component" % PROTOCOL_NAME
-
-        self.conf = conf
-        self.app = app
-        #if app is set, then we are in a WSGI pipeline and requests get passed
-        # on to app. If it is not set, this component should forward requests
-
-        # where to find the OpenStack service (if not in local WSGI chain)
-        # these settings are only used if this component is acting as a proxy
-        # and the OpenSTack service is running remotely
-        self.service_protocol = conf.get('service_protocol', 'https')
-        self.service_host = conf.get('service_host')
-        self.service_port = int(conf.get('service_port'))
-        self.service_url = '%s://%s:%s' % (self.service_protocol,
-                                           self.service_host,
-                                           self.service_port)
-        # used to verify this component with the OpenStack service or PAPIAuth
-        self.service_pass = conf.get('service_pass')
-
-        # delay_auth_decision means we still allow unauthenticated requests
-        # through and we let the downstream service make the final decision
-        self.delay_auth_decision = int(conf.get('delay_auth_decision', 0))
-
-    def _init_protocol(self, conf):
-        """ Protocol specific initialization """
-
-        # where to find the auth service (we use this to validate tokens)
-        self.auth_host = conf.get('auth_host')
-        self.auth_port = int(conf.get('auth_port'))
-        self.auth_protocol = conf.get('auth_protocol', 'https')
-
-        # where to tell clients to find the auth service (default to url
-        # constructed based on endpoint we have for the service to use)
-        self.auth_location = conf.get('auth_uri',
-                                        "%s://%s:%s" % (self.auth_protocol,
-                                        self.auth_host,
-                                        self.auth_port))
-
-        # Credentials used to verify this component with the Auth service since
-        # validating tokens is a privileged call
-        self.admin_token = conf.get('admin_token')
-
     def __init__(self, app, conf):
         """ Common initialization code """
-
-        #TODO(ziad): maybe we refactor this into a superclass
-        #self._init_protocol_common(app, conf)  # Applies to all protocols
-        #self._init_protocol(conf)  # Specific to this protocol
 
         # add by colony. conf tokens be simple.
         self.conf = conf
@@ -153,7 +105,6 @@ class AuthProtocol(object):
         # for ACL setting for containers of an account which others possesses.
         self.across_account = conf.get('across_account', 'yes').lower() in TRUE_VALUES
         self.memcache_expire = float(conf.get('memcache_expire', 86400))
-        self.easy_s3_auth = False
 
     def __call__(self, env, start_response):
         """ Handle incoming request. Authenticate. And send downstream. """
@@ -182,14 +133,8 @@ class AuthProtocol(object):
         if not claims:
             return self._reject_request(env, start_response)
 
-        # s3 support. add by colony
-        s3 = env.get('HTTP_AUTHORIZATION')
-        ep_path = env.get('HTTP_X_ENDPOINT_PATH')
-        if s3 and self.easy_s3_auth and not ep_path :
-            result = self._s3_auth(env, claims)
-        else:
-            # check auth token with no admin privilege. add by colony
-            result = self._accession_by_auth_token(env, claims)
+        # check auth token with no admin privilege. add by colony
+        result = self._accession_by_auth_token(env, claims)
 
         if not result:
             return self._reject_request(env, start_response)
@@ -208,79 +153,6 @@ class AuthProtocol(object):
         env['swift.clean_acl'] = clean_acl
         return self.app(env, start_response)
 
-        #Look for authentication claims
-        # claims = self._get_claims(env)
-        # if not claims:
-        #     #No claim(s) provided
-        #     if self.delay_auth_decision:
-        #         #Configured to allow downstream service to make final decision.
-        #         #So mark status as Invalid and forward the request downstream
-        #         self._decorate_request("X_IDENTITY_STATUS",
-        #             "Invalid", env, proxy_headers)
-        #     else:
-        #         #Respond to client as appropriate for this auth protocol
-        #         return self._reject_request(env, start_response)
-        # else:
-        #     #this request is presenting claims. Let's validate them
-        #     valid = self._validate_claims(claims)
-        #     if not valid:
-        #         # Keystone rejected claim
-        #         if self.delay_auth_decision:
-        #             # Downstream service will receive call still and decide
-        #             self._decorate_request("X_IDENTITY_STATUS",
-        #                 "Invalid", env, proxy_headers)
-        #         else:
-        #             #Respond to client as appropriate for this auth protocol
-        #             return self._reject_claims(env, start_response)
-        #     else:
-        #         self._decorate_request("X_IDENTITY_STATUS",
-        #             "Confirmed", env, proxy_headers)
-        #     #Collect information about valid claims
-        #     if valid:
-        #         if not self._verify_auth_token(req, claims):
-        #             return self._reject_request(env, start_response)
-        #         claims = self._expound_claims(claims)
-
-        #         # Store authentication data
-        #         if claims:
-        #             self._decorate_request('X_AUTHORIZATION', "Proxy %s" %
-        #                 claims['user'], env, proxy_headers)
-        #             self._decorate_request('X_TENANT',
-        #                 claims['tenant'], env, proxy_headers)
-        #             self._decorate_request('X_USER',
-        #                 claims['user'], env, proxy_headers)
-        #             if 'roles' in claims and len(claims['roles']) > 0:
-        #                 if claims['roles'] != None:
-        #                     roles = ''
-        #                     for role in claims['roles']:
-        #                         if len(roles) > 0:
-        #                             roles += ','
-        #                         roles += role
-        #                     self._decorate_request('X_ROLE',
-        #                         roles, env, proxy_headers)
-        #             # NOTE(todd): unused
-        #             self.expanded = True
-        # #Send request downstream
-        # return self._forward_request(env, start_response, proxy_headers)
-
-    # NOTE(todd): unused
-    def get_admin_auth_token(self, username, password):
-        """
-        This function gets an admin auth token to be used by this service to
-        validate a user's token. Validate_token is a priviledged call so
-        it needs to be authenticated by a service that is calling it
-        """
-        headers = {"Content-type": "application/json", "Accept": "text/json"}
-        params = {"passwordCredentials": {"username": username,
-                                          "password": password,
-                                          "tenantId": "1"}}
-        conn = httplib.HTTPConnection("%s:%s" \
-            % (self.auth_host, self.auth_port))
-        conn.request("POST", "/v2.0/tokens", json.dumps(params), \
-            headers=headers)
-        response = conn.getresponse()
-        data = response.read()
-        return data
 
     def _get_claims(self, env):
         """Get claims from request"""
@@ -294,129 +166,11 @@ class AuthProtocol(object):
                       "Keystone uri='%s'" % self.auth_location)])(env,
                                                         start_response)
 
-    def _reject_claims(self, env, start_response):
-        """Client sent bad claims"""
-        return HTTPUnauthorized()(env,
-            start_response)
-
-
-    def _validate_claims(self, claims):
-        """Validate claims, and provide identity information isf applicable """
-
-        # Step 1: We need to auth with the keystone service, so get an
-        # admin token
-        #TODO(ziad): Need to properly implement this, where to store creds
-        # for now using token from ini
-        #auth = self.get_admin_auth_token("admin", "secrete", "1")
-        #admin_token = json.loads(auth)["auth"]["token"]["id"]
-
-        # Step 2: validate the user's token with the auth service
-        # since this is a priviledged op,m we need to auth ourselves
-        # by using an admin token
-        headers = {"Content-type": "application/json",
-                    "Accept": "text/json",
-                    "X-Auth-Token": self.admin_token}
-                    ##TODO(ziad):we need to figure out how to auth to keystone
-                    #since validate_token is a priviledged call
-                    #Khaled's version uses creds to get a token
-                    # "X-Auth-Token": admin_token}
-                    # we're using a test token from the ini file for now
-        conn = http_connect(self.auth_host, self.auth_port, 'GET',
-                            '/v2.0/tokens/%s' % claims, headers=headers)
-        resp = conn.getresponse()
-        # data = resp.read()
-        conn.close()
-
-        if not str(resp.status).startswith('20'):
-            # Keystone rejected claim
-            return False
-        else:
-            #TODO(Ziad): there is an optimization we can do here. We have just
-            #received data from Keystone that we can use instead of making
-            #another call in _expound_claims
-            return True
-
-    def _expound_claims(self, claims):
-        # Valid token. Get user data and put it in to the call
-        # so the downstream service can use it
-        headers = {"Content-type": "application/json",
-                    "Accept": "text/json",
-                    "X-Auth-Token": self.admin_token}
-                    ##TODO(ziad):we need to figure out how to auth to keystone
-                    #since validate_token is a priviledged call
-                    #Khaled's version uses creds to get a token
-                    # "X-Auth-Token": admin_token}
-                    # we're using a test token from the ini file for now
-        conn = http_connect(self.auth_host, self.auth_port, 'GET',
-                            '/v2.0/tokens/%s' % claims, headers=headers)
-        resp = conn.getresponse()
-        data = resp.read()
-        conn.close()
-
-        if not str(resp.status).startswith('20'):
-            raise LookupError('Unable to locate claims: %s' % resp.status)
-
-        token_info = json.loads(data)
-        #print token_info
-        roles = []
-        role_refs = token_info["access"]["user"]["roles"]
-        if role_refs != None:
-            for role_ref in role_refs:
-                roles.append(role_ref["name"])
-
-        try:
-            tenant = token_info['access']['token']['tenant']['Id']
-        except:
-            tenant = None
-        if not tenant:
-            tenant = token_info['access']['user']['tenantName']
-        verified_claims = {'user': token_info['access']['user']['username'],
-                    'tenant': tenant,
-                    'roles': roles}
-        return verified_claims
-
     def _decorate_request(self, index, value, env, proxy_headers):
         """Add headers to request"""
         proxy_headers[index] = value
         env["HTTP_%s" % index] = value
 
-    def _forward_request(self, env, start_response, proxy_headers):
-        """Token/Auth processed & claims added to headers"""
-        self._decorate_request('AUTHORIZATION',
-            "Basic %s" % self.service_pass, env, proxy_headers)
-        #now decide how to pass on the call
-        if self.app:
-            # Pass to downstream WSGI component
-            return self.app(env, start_response)
-            #.custom_start_response)
-        else:
-            # We are forwarding to a remote service (no downstream WSGI app)
-            req = Request(proxy_headers)
-            parsed = urlparse(req.url)
-
-            conn = http_connect(self.service_host,
-                                self.service_port,
-                                req.method,
-                                parsed.path,
-                                proxy_headers,
-                                ssl=(self.service_protocol == 'https'))
-            resp = conn.getresponse()
-            data = resp.read()
-
-            #TODO(ziad): use a more sophisticated proxy
-            # we are rewriting the headers now
-
-            if resp.status == 401 or resp.status == 305:
-                # Add our own headers to the list
-                headers = [("WWW_AUTHENTICATE",
-                   "Keystone uri='%s'" % self.auth_location)]
-                return Response(status=resp.status, body=data,
-                            headerlist=headers)(env,
-                                                start_response)
-            else:
-                return Response(status=resp.status, body=data)(env,
-                                                start_response)
-    
 
     def _validate_claims_each_user(self, auth_user):
         """ 
@@ -442,7 +196,7 @@ class AuthProtocol(object):
         auth_req = {'auth': {'passwordCredentials': {'username': user, 'password': password}}}
         req_headers = {"Content-type": "application/json", "Accept": "text/json"}
         connect = httplib.HTTPConnection if self.auth_protocol == 'http' else httplib.HTTPSConnection
-        conn = connect('%s' % self.auth_netloc)
+        conn = connect('%s' % self.auth_netloc, timeout=10)
         conn.request('POST', '/v2.0/tokens', json.dumps(auth_req), req_headers)
         resp = conn.getresponse()
         #print resp.status
@@ -649,42 +403,6 @@ class AuthProtocol(object):
             return HTTPForbidden(request=req)
         else:
             return HTTPUnauthorized(request=req)
-
-
-    def _s3_auth(self, env, token):
-        """
-        add by colony
-
-        AWS S3 API support. co-working with swift3.py.
-
-        Authorization header is assumed,
-         'AWS account:user:password:sign'
-        like 'Authorization: AWS test:tester:testing:ZqDmuA7PLBtw6Qrl/nJWLyGX5Ck='
-
-        in s3curl as:
-        $ s3curl.pl --id test:tester:testing --key testing -- http://192.168.2.1:8080/TEST0
-
-        """
-        account = env['HTTP_AUTHORIZATION'].split(' ')[1]
-        try:
-            tenant, user, password, sign = account.split(':')
-        except ValueError:
-            return None
-        msg = base64.urlsafe_b64decode(unquote(token))
-        s = base64.encodestring(hmac.new(password, msg, sha1).digest()).strip()
-        # print 'tenant: %s, user: %s, sign: %s' % (tenant, user, sign)
-        # print 'msg: %s' % msg
-        # print 'sign: %s' % sign
-        # print 's: %s' % s
-        if sign != s:
-            return None
-        auth_resp = self._authreq_to_keystone(user, password)
-        auth_token, auth_tenant, username, roles, storage_url = self._get_swift_info(auth_resp, self.region_name) 
-        if auth_tenant != tenant:
-            return None
-        if not storage_url:
-            return None
-        return tenant, username, roles, storage_url 
 
 
 def filter_factory(global_conf, **local_conf):
