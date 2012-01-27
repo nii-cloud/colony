625156a5909038fe55ca282983d84a61e6c94a4f
Motonobu Ichimura <motonobu@gmail.com>
2012-01-27 13:06:34 +0900

    * bug fixed

--- installer/tools/with_venv.sh
+++ installer/tools/with_venv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 TOOLS=`dirname $0`
-if test -n $COLONY_INSTALL_HOME; then
+if test "x$COLONY_INSTALL_HOME" != "x"; then
     VENV=$COLONY_INSTALL_HOME
 else
     VENV=$TOOLS/../.installer-venv
 fi
+
 source $VENV/bin/activate && $@
